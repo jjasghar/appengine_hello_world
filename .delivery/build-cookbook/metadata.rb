4622f04419976129e860ddee93a88a6a2545b867
JJ Asghar <jj@chef.io>
2016-06-22 17:52:02 -0500

    fixed apt

--- .delivery/build-cookbook/metadata.rb
+++ .delivery/build-cookbook/metadata.rb
@@ -5,5 +5,4 @@ license 'all_rights'
 version '0.1.0'
 
 depends 'appengine'
-depends 'apt'
 depends 'poise-python'
--- .delivery/build-cookbook/recipes/default.rb
+++ .delivery/build-cookbook/recipes/default.rb
@@ -4,6 +4,14 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-include_recipe "apt::default"
+execute "apt-get-update-periodic" do
+  command "apt-get update"
+  ignore_failure true
+  only_if do
+    File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
+    File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400
+  end
+end
+
 include_recipe "appengine::default"
 include_recipe "poise-python::default"
