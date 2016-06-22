4260c76670c3327707421dc512b0b47183f87558
JJ Asghar <jj@chef.io>
2016-06-22 18:00:42 -0500

    added pep8

--- .delivery/build-cookbook/recipes/lint.rb
+++ .delivery/build-cookbook/recipes/lint.rb
@@ -5,3 +5,25 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 include_recipe "build-cookbook::default"
+
+src_dir = File.expand_path("#{node['delivery']['workspace']['repo']}/")
+
+bash "install pep8" do
+  user "root"
+  cwd src_dir
+  code <<-EOH
+    STATUS=0
+    pip install pep8  || STATUS=1
+    exit $STATUS
+  EOH
+end
+
+bash "check the python script" do
+  user "root"
+  cwd src_dir
+  code <<-EOH
+    STATUS=0
+    pep8 --first main.py  || STATUS=1
+    exit $STATUS
+  EOH
+end
