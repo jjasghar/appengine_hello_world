5f181baf619c5d002f82bb0a74f69b7574aadecb
JJ Asghar <jj@chef.io>
2016-06-24 12:53:21 -0500

    fixing up things

--- .delivery/build-cookbook/metadata.rb
+++ .delivery/build-cookbook/metadata.rb
@@ -4,5 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 version '0.1.0'
 
+depends 'apt'
 depends 'appengine'
 depends 'poise-python'
--- .delivery/build-cookbook/test/fixtures/cookbooks/test/recipes/default.rb
+++ .delivery/build-cookbook/test/fixtures/cookbooks/test/recipes/default.rb
@@ -1,3 +1,14 @@
+cmd = "HOME=/home/vagrant delivery job verify default"
+cmd <<  ' --server localhost --ent test --org kitchen'
+execute cmd do
+  cwd '/tmp/repo-data'
+  user 'root'
+end
+
+execute 'fix perms' do
+  command 'chown -R vagrant /home/vagrant/.berkshelf'
+end
+
 %w(unit lint syntax).each do |phase|
   # TODO: This works on Linux/Unix. Not Windows.
   execute "HOME=/home/vagrant delivery job verify #{phase} --server localhost --ent test --org kitchen" do
