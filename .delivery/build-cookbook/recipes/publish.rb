9952329592067a5eef0eec5a60f796c4d9ffef22
JJ Asghar <jj@chef.io>
2016-06-30 13:13:47 -0500

    updates

--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -3,3 +3,11 @@
 # Recipe:: publish
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+appengine 'formal-platform-134918' do
+  app_yaml "#{node.default['appengine']['source_location']}/app.yaml"
+  service_id 'default'
+  bucket_name 'chef-conf16-appengine'
+  service_account_json account_json
+  source node.default['appengine']['source_location']
+end
