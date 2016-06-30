fc46a82c12ec832d72242a8c561926e37bd3af6e
JJ Asghar <jj@chef.io>
2016-06-30 15:56:34 -0500

    ubuntu and rhel not playing nice

--- .delivery/build-cookbook/metadata.rb
+++ .delivery/build-cookbook/metadata.rb
@@ -2,7 +2,7 @@ name 'build-cookbook'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'all_rights'
-version '0.1.24'
+version '0.1.25'
 
 depends 'apt'
 depends 'appengine'
--- .delivery/build-cookbook/recipes/default.rb
+++ .delivery/build-cookbook/recipes/default.rb
@@ -28,22 +28,22 @@ bash "install pip" do
     rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-7.noarch.rpm || STATUS=1
     exit $STATUS
   EOH
-  only_if { node.default['platform'] = 'redhat' || node.default['platform'] = 'centos' }
+  only_if { node['platform'] = 'redhat' || node['platform'] = 'centos' }
 end
 
 package 'python-devel' do
   action :install
-  only_if { node.default['platform'] = 'redhat' || node.default['platform'] = 'centos' }
+  only_if { node['platform'] = 'redhat' || node['platform'] = 'centos' }
 end
 
 package 'python-cffi' do
   action :install
-  only_if { node.default['platform'] = 'redhat' || node.default['platform'] = 'centos' }
+  only_if { node['platform'] = 'redhat' || node['platform'] = 'centos' }
 end
 
 package 'python-pip' do
   action :install
-  only_if { node.default['platform'] = 'redhat' || node.default['platform'] = 'centos' }
+  only_if { node['platform'] = 'redhat' || node['platform'] = 'centos' }
 end
 
 bash "install pep8" do
