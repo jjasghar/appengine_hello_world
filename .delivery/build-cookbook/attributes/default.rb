bb2f7898d49450cd95b3eaeaec3077d1516b3321
JJ Asghar <jj@chef.io>
2016-06-30 12:40:36 -0500

    updates pulling down 0.0.7

---
+++ .delivery/build-cookbook/attributes/default.rb
@@ -0,0 +1,11 @@
+# Location to clone the code from
+default['appengine']['repository'] = 'https://github.com/jjasghar/appengine_hello_world'
+
+# Location for the source repo to clone the repository
+default['appengine']['source_location'] = '/tmp/hello_world'
+
+# branch to clone the code from
+default['appengine']['branch'] = 'master'
+
+# demo of appengine
+default['appengine']['demo'] = true
