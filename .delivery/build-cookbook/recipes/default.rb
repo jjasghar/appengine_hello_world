a8c71af8214f99b124ffc5042cacd6dfa4dcbd30
JJ Asghar <jj@chef.io>
2016-06-30 15:25:34 -0500

    forgot the -y

--- .delivery/build-cookbook/recipes/default.rb
+++ .delivery/build-cookbook/recipes/default.rb
@@ -24,7 +24,7 @@ bash "install pip" do
   cwd src_dir
   code <<-EOH
     STATUS=0
-    yum install python-cffi
+    yum install -y python-cffi
     rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-7.noarch.rpm || STATUS=1
     exit $STATUS
   EOH
