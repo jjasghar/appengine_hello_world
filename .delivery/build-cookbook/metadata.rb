463fe59911539a34b938f117cc44bf61820bf82f
JJ Asghar <jj@chef.io>
2016-06-30 13:26:32 -0500

    moved pep8 install

--- .delivery/build-cookbook/metadata.rb
+++ .delivery/build-cookbook/metadata.rb
@@ -2,7 +2,7 @@ name 'build-cookbook'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'all_rights'
-version '0.1.9'
+version '0.1.10'
 
 depends 'apt'
 depends 'appengine'
--- .delivery/build-cookbook/recipes/default.rb
+++ .delivery/build-cookbook/recipes/default.rb
@@ -4,5 +4,18 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+src_dir = File.expand_path("#{node['delivery']['workspace']['repo']}/")
+
 include_recipe "appengine::default"
 include_recipe "poise-python::default"
+
+bash "install pep8" do
+  user 'root'
+  cwd src_dir
+  code <<-EOH
+    STATUS=0
+    pip install --upgrade ndg-httpsclient || STATUS=1
+    pip install pep8  || STATUS=1
+    exit $STATUS
+  EOH
+end
