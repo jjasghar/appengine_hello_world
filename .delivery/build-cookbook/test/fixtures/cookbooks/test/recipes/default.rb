3554fff2f983e0dcf5197f2fa53e92875e525cb4
JJ Asghar <jj@chef.io>
2016-06-30 17:36:08 -0500

    added publish to test cookbook

--- .delivery/build-cookbook/test/fixtures/cookbooks/test/recipes/default.rb
+++ .delivery/build-cookbook/test/fixtures/cookbooks/test/recipes/default.rb
@@ -9,7 +9,7 @@ execute 'fix perms' do
   command 'chown -R vagrant /home/vagrant/.berkshelf'
 end
 
-%w(unit lint syntax).each do |phase|
+%w(unit lint syntax publish).each do |phase|
   # TODO: This works on Linux/Unix. Not Windows.
   execute "HOME=/home/vagrant delivery job verify #{phase} --server localhost --ent test --org kitchen" do
     cwd '/tmp/repo-data'
