6b3e659c517642617dd6a1244019193a58a1d2cd
JJ Asghar <jj@chef.io>
2016-07-05 17:13:42 -0500

    added the src directory

--- .delivery/build-cookbook/recipes/deploy.rb
+++ .delivery/build-cookbook/recipes/deploy.rb
@@ -9,7 +9,7 @@ account_json = ::File.expand_path('/tmp/gcloud/service_account.json')
 src_dir = File.expand_path("#{node['delivery']['workspace']['repo']}")
 
 appengine 'formal-platform-134918' do
-  app_yaml "#{node.default['appengine']['source_location']}/app.yaml"
+  app_yaml "#{src_dir}/#{node.default['appengine']['source_location']}/app.yaml"
   service_id 'default'
   bucket_name 'chef-conf16-appengine'
   service_account_json account_json
