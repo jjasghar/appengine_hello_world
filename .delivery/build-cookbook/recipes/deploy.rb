8e3d1017d38daa4cb88fdc55f71d7b6cf3f5fb86
JJ Asghar <jj@chef.io>
2016-07-05 18:24:19 -0500

    conditional logic

--- .delivery/build-cookbook/recipes/deploy.rb
+++ .delivery/build-cookbook/recipes/deploy.rb
@@ -8,18 +8,39 @@ account_json = ::File.expand_path('/tmp/gcloud/service_account.json')
 
 src_dir = File.expand_path("#{node['delivery']['workspace']['repo']}")
 
-git "#{src_dir}/#{node.default['appengine']['source_location']}" do
-  repository node.default['appengine']['repository']
-  reference  node.default['appengine']['branch']
-  user 'dbuild'
-  group 'dbuild'
-  action :sync
-end
+case node['delivery']['change']['stage']
+when 'acceptance'
+  git "#{src_dir}/#{node.default['appengine']['source_location']}" do
+    repository node.default['appengine']['repository']
+    reference  node.default['appengine']['branch']
+    user 'dbuild'
+    group 'dbuild'
+    action :sync
+  end
 
-appengine 'formal-platform-134918' do
-  app_yaml "#{src_dir}/#{node.default['appengine']['source_location']}/app.yaml"
-  service_id 'default'
-  bucket_name 'chef-conf16-appengine'
-  service_account_json account_json
-  source "#{src_dir}/#{node.default['appengine']['source_location']}"
+  appengine 'formal-platform-134918' do
+    app_yaml "#{src_dir}/#{node.default['appengine']['source_location']}/app.yaml"
+    service_id 'default'
+    bucket_name 'chef-conf16-appengine'
+    service_account_json account_json
+    source "#{src_dir}/#{node.default['appengine']['source_location']}"
+    action :stage
+  end
+
+when 'delivered'
+  git "#{src_dir}/#{node.default['appengine']['source_location']}" do
+    repository node.default['appengine']['repository']
+    reference  node.default['appengine']['branch']
+    user 'dbuild'
+    group 'dbuild'
+    action :sync
+  end
+
+  appengine 'formal-platform-134918' do
+    app_yaml "#{src_dir}/#{node.default['appengine']['source_location']}/app.yaml"
+    service_id 'default'
+    bucket_name 'chef-conf16-appengine'
+    service_account_json account_json
+    source "#{src_dir}/#{node.default['appengine']['source_location']}"
+  end
 end
