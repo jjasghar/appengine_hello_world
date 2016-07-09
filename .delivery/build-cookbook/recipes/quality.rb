ba3db44556654217448039e19afae0219b292ad7
Nelson Araujo <nelsona@google.com>
2016-07-09 12:46:29 -0700

    Adding quality phase

--- .delivery/build-cookbook/recipes/quality.rb
+++ .delivery/build-cookbook/recipes/quality.rb
@@ -3,3 +3,15 @@
 # Recipe:: quality
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# TODO(jj): Extract the dynamic URL from the appengine resource
+staging_site_url = 'https://www.google.com'
+
+bash "check site images integrity" do
+  cwd src_dir
+  code <<-EOH
+    STATUS=0
+    ruby scripts/site_image_tester.rb #{staging_site_url} || STATUS=1
+    exit $STATUS
+  EOH
+end
