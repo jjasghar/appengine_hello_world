8f6097f3258a03986915a9de6a49c68e55f425ce
Nelson Araujo <nelsona@google.com>
2016-07-09 15:05:02 -0700

    Adding security tests to site delivery.

--- .delivery/build-cookbook/recipes/quality.rb
+++ .delivery/build-cookbook/recipes/quality.rb
@@ -11,7 +11,8 @@ bash "check site images integrity" do
   cwd src_dir
   code <<-EOH
     STATUS=0
-    ruby scripts/site_image_tester.rb #{staging_site_url} || STATUS=1
+    IMAGE_TESTER_AVAILABILITY_TESTS=1 \
+      ruby scripts/site_image_tester.rb #{staging_site_url} || STATUS=1
     exit $STATUS
   EOH
 end
--- .delivery/build-cookbook/scripts/site_image_tester.rb
+++ .delivery/build-cookbook/scripts/site_image_tester.rb
@@ -13,18 +13,25 @@
 # limitations under the License.
 
 # Public: This program takes 1 argument (URL) to inspect. It downloads the HTML
-# and fetches each image in the body content, checking for its existance.
+# and fetches each image in the body content, checking for its existance and/or
+# testing its security.
 
-# Usage #1:
-#     ruby site_image_tester.rb 'https://www.google.com' \
+# Usage:
+#
+#     [IMAGE_TESTER_?????? ...] ruby site_image_tester.rb \
+#         'https://www.google.com' \
 #       && echo 'OK' \
 #       || echo 'FAILED'
-
-# Usage #2 (optionally you can fail if no images are found:
-#     IMAGE_TESTER_FAIL_IF_NO_IMAGES=1 ruby site_image_tester.rb \
-#         'https://images.google.com' \
-#       && echo 'OK' \
-#       || echo 'FAILED'
+#
+#   where ????? can be one of the following:
+#
+#     IMAGE_TESTER_SECURITY_TESTS     : Performs security tests on the URL
+#     IMAGE_TESTER_AVAILABILITY_TESTS : Performs availability checks for the
+#                                       images
+#
+#   optional behavior control:
+#
+#     IMAGE_TESTER_FAIL_IF_NO_IMAGES  : Fails the test if the URL has no images
 
 require 'net/http'
 require 'net/https'
@@ -33,9 +40,12 @@ require 'open-uri'
 require 'uri'
 
 healthy = true
+tests_run = false
 
-fail_if_no_images = ENV['IMAGE_TESTER_FAIL_IF_NO_IMAGES']
 site = ARGV[0]
+fail_if_no_images = ENV['IMAGE_TESTER_FAIL_IF_NO_IMAGES']
+security_tests = ENV['IMAGE_TESTER_SECURITY_TESTS']
+availability_tests = ENV['IMAGE_TESTER_AVAILABILITY_TESTS']
 
 doc = Nokogiri::HTML(open(site))
 images = doc.xpath("//img/@src")
@@ -50,19 +60,43 @@ if images.length == 0 then
   end
 end
 
-doc.xpath("//img/@src").each do |src|
-  uri = URI.join(site, src)
-  print "Checking image #{uri}... "
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = uri.instance_of? URI::HTTPS
-  head = Net::HTTP::Head.new(uri.request_uri)
-  response = http.request(head)
-  if response.instance_of? Net::HTTPOK then
-    puts "OK"
-  else
-    puts "FAILED #{response.class.name.gsub('Net::HTTP', '')}"
-    healthy = false
+if security_tests then
+  tests_run = true
+  doc.xpath("//img/@src").each do |src|
+    uri = URI.join(site, src)
+    next if uri.scheme != 'http' and uri.scheme != 'https'
+    print "Checking security for image #{uri}... "
+    if uri.scheme == 'https' then
+      puts "OK"
+    else
+      puts "FAILED (insecure image)"
+      healthy = false
+    end
   end
+end
+
+if availability_tests then
+  tests_run = true
+  doc.xpath("//img/@src").each do |src|
+    uri = URI.join(site, src)
+    next if uri.scheme != 'http' and uri.scheme != 'https'
+    print "Checking availability of image #{uri}... "
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = uri.instance_of? URI::HTTPS
+    head = Net::HTTP::Head.new(uri.request_uri)
+    response = http.request(head)
+    if response.instance_of? Net::HTTPOK then
+      puts "OK"
+    else
+      puts "FAILED #{response.class.name.gsub('Net::HTTP', '')}"
+      healthy = false
+    end
+  end
+end
+
+if not tests_run then
+  puts "FATAL: No tests were run."
+  exit 1
 end
 
 exit healthy ? 0 : 1
