dcd04e6f742b19cf18cb16495ddb65e13da947a1
Nelson Araujo <nelsona@google.com>
2016-07-11 16:03:42 -0700

    Enable printing output to Delivery.

--- .delivery/build-cookbook/metadata.rb
+++ .delivery/build-cookbook/metadata.rb
@@ -2,7 +2,7 @@ name 'build-cookbook'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'all_rights'
-version '0.1.36'
+version '0.1.38'
 
 depends 'apt'
 depends 'appengine'
--- .delivery/build-cookbook/recipes/functional.rb
+++ .delivery/build-cookbook/recipes/functional.rb
@@ -9,12 +9,22 @@ src_dir = File.expand_path("#{node['delivery']['workspace']['repo']}")
 # TODO(jj): Extract the dynamic URL from the appengine resource
 staging_site_url = 'https://www.google.com'
 
+image_checker_logfile="/tmp/build-functional-#{ENV['CHEF_PUSH_JOB_ID']}.log"
+
 bash "check site images integrity" do
   cwd src_dir
   code <<-EOH
     STATUS=0
     IMAGE_TESTER_AVAILABILITY_TESTS=1 \
-    /opt/chefdk/embedded/bin/ruby .delivery/build-cookbook/scripts/site_image_tester.rb #{staging_site_url} || STATUS=1
+      /opt/chefdk/embedded/bin/ruby \
+        .delivery/build-cookbook/scripts/site_image_tester.rb \
+          #{staging_site_url} > #{image_checker_logfile}
+    STATUS=$?
     exit $STATUS
   EOH
 end
+
+output=`cat #{image_checker_logfile}`
+Chef::Log.info output
+
+File.delete(image_checker_logfile)
