bfc1291a491606e526a21b32e0b5d428264ae8c6
JJ Asghar <jj@chef.io>
2016-07-11 14:47:09 -0500

    more fixes

--- .delivery/build-cookbook/recipes/quality.rb
+++ .delivery/build-cookbook/recipes/quality.rb
@@ -14,12 +14,8 @@ bash "check site images integrity" do
   cwd src_dir
   code <<-EOH
     STATUS=0
-    ls -l ../../../
-    ls -l ../../../../
-    ls -l ../../../../../
-    ls -l ../../../../chef
     IMAGE_TESTER_AVAILABILITY_TESTS=1 \
-      ruby ../../../../chef/build-cookbook/scripts/site_image_tester.rb #{staging_site_url} || STATUS=1
+      ruby ../../chef/build-cookbook/scripts/site_image_tester.rb #{staging_site_url} || STATUS=1
     exit $STATUS
   EOH
 end
