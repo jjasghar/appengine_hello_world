869a48df783b5b5ae06639746ef06d744e74606f
JJ Asghar <jj@chef.io>
2016-07-11 17:19:00 -0500

    new version of stuff

--- .delivery/build-cookbook/recipes/functional.rb
+++ .delivery/build-cookbook/recipes/functional.rb
@@ -4,6 +4,8 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+src_dir = File.expand_path("#{node['delivery']['workspace']['repo']}")
+
 # TODO(jj): Extract the dynamic URL from the appengine resource
 staging_site_url = 'https://www.google.com'
 
@@ -12,7 +14,7 @@ bash "check site images integrity" do
   code <<-EOH
     STATUS=0
     IMAGE_TESTER_AVAILABILITY_TESTS=1 \
-      ruby scripts/site_image_tester.rb #{staging_site_url} || STATUS=1
+    /opt/chefdk/embedded/bin/ruby scripts/site_image_tester.rb #{staging_site_url} || STATUS=1
     exit $STATUS
   EOH
 end
