85c725d1dfc4b03832a4430b945a8eb54b0aaf36
JJ Asghar <jj@chef.io>
2016-07-11 13:42:23 -0500

    pwd

--- .delivery/build-cookbook/recipes/quality.rb
+++ .delivery/build-cookbook/recipes/quality.rb
@@ -14,6 +14,7 @@ bash "check site images integrity" do
   cwd src_dir
   code <<-EOH
     STATUS=0
+    pwd
     IMAGE_TESTER_AVAILABILITY_TESTS=1 \
       ruby build-cookbook/scripts/site_image_tester.rb #{staging_site_url} || STATUS=1
     exit $STATUS
