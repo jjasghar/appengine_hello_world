17423a7db21a2623c90f05d69e40025682d4e953
Nelson Araujo <nelsona@google.com>
2016-07-11 16:48:44 -0700

    Constrain test to acceptance phase.

--- .delivery/build-cookbook/metadata.rb
+++ .delivery/build-cookbook/metadata.rb
@@ -2,7 +2,7 @@ name 'build-cookbook'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'all_rights'
-version '0.1.43'
+version '0.1.44'
 
 depends 'apt'
 depends 'appengine'
--- .delivery/build-cookbook/recipes/functional.rb
+++ .delivery/build-cookbook/recipes/functional.rb
@@ -11,25 +11,28 @@ staging_site_url = 'https://www.google.com'
 
 image_checker_logfile="/tmp/build-functional-#{ENV['CHEF_PUSH_JOB_ID']}.log"
 
-bash "check site images integrity" do
-  cwd src_dir
-  code <<-EOH
-    STATUS=0
-    IMAGE_TESTER_AVAILABILITY_TESTS=1 \
-      /opt/chefdk/embedded/bin/ruby \
-        .delivery/build-cookbook/scripts/site_image_tester.rb \
-          #{staging_site_url} > #{image_checker_logfile}
-    STATUS=$?
-    exit $STATUS
-  EOH
-end
-
-ruby_block 'print integrity checks output' do
-  block do
-    output=`cat #{image_checker_logfile}`
-    Chef::Log.info output
-
-    File.delete(image_checker_logfile)
+case node['delivery']['change']['stage']
+when 'acceptance'
+  bash "check site images integrity" do
+    cwd src_dir
+    code <<-EOH
+      STATUS=0
+      IMAGE_TESTER_AVAILABILITY_TESTS=1 \
+        /opt/chefdk/embedded/bin/ruby \
+          .delivery/build-cookbook/scripts/site_image_tester.rb \
+            #{staging_site_url} > #{image_checker_logfile}
+      STATUS=$?
+      exit $STATUS
+    EOH
   end
-  action :run
+
+  ruby_block 'print integrity checks output' do
+    block do
+      output=`cat #{image_checker_logfile}`
+      Chef::Log.info output
+
+      File.delete(image_checker_logfile)
+    end
+    action :run
+  end
 end
