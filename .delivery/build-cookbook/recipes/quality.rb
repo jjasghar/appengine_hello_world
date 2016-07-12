5c3e78c704bf5a32cd5547f6dacd8e8f04af6525
Nelson Araujo <nelsona@google.com>
2016-07-11 17:19:19 -0700

    Disable quality phase

--- .delivery/build-cookbook/recipes/quality.rb
+++ .delivery/build-cookbook/recipes/quality.rb
@@ -3,20 +3,3 @@
 # Recipe:: quality
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
-
-# TODO(jj): Extract the dynamic URL from the appengine resource
-
-src_dir = File.expand_path("#{node['delivery']['workspace']['repo']}")
-
-staging_site_url = 'https://www.google.com'
-
-bash "check site images integrity" do
-  cwd src_dir
-  code <<-EOH
-    STATUS=0
-    export
-    IMAGE_TESTER_AVAILABILITY_TESTS=1 \
-       /opt/chefdk/embedded/bin/ruby .delivery/build-cookbook/scripts/site_image_tester.rb #{staging_site_url} || STATUS=1
-    exit $STATUS
-  EOH
-end
