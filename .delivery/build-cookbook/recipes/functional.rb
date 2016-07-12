dc30a516662f363d8abbd1a9d9d1a56e90002705
JJ Asghar <jj@chef.io>
2016-07-12 11:23:35 -0500

    converted to execute instead of bash

--- .delivery/build-cookbook/recipes/functional.rb
+++ .delivery/build-cookbook/recipes/functional.rb
@@ -20,15 +20,13 @@ puts "Application version #{deployer.staging_url}"
 
 case node['delivery']['change']['stage']
 when 'acceptance'
-  bash "check site images integrity" do
+  execute "check site images integrity" do
     cwd src_dir
-    code <<-EOH
-      STATUS=0
+    command <<-EOF
       IMAGE_TESTER_AVAILABILITY_TESTS=1 \
         /opt/chefdk/embedded/bin/ruby \
           .delivery/build-cookbook/scripts/site_image_tester.rb \
-            #{deployer.staging_url} || STATUS=1
-      exit $STATUS
-    EOH
+            #{deployer.staging_url}
+    EOF
   end
 end
